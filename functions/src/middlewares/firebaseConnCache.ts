e06e13b71014fe13f573a530e079bd50130017d5
jscht <80654820+jscht@users.noreply.github.com>
2024-09-25 23:05:46 +0900

    Add: 파이어베이스 연결 확인 미들웨어 추가
    30분 간격 연결 확인

---
+++ functions/src/middlewares/firebaseConnCache.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { logger } from "firebase-functions";
+import { checkFirebaseConnection } from "../providers/firebase";
+
+// redis로 변경
+let fbLastCheckTime: number | null = null;
+
+/**
+ * @description 30분 간격 파이어베이스 연결 확인
+ */
+export async function firebaseConnCache(req: Request, res: Response, next: NextFunction) {
+  const currentTime = Date.now();
+  if (fbLastCheckTime && (currentTime - fbLastCheckTime < 1800000)) {
+    logger.info("Using cached connection");
+    console.log("Using cached connection");
+    return next();
+  }
+
+  const firestoreReady = await checkFirebaseConnection();
+
+  if (firestoreReady) {
+    fbLastCheckTime = currentTime;
+    return next();
+  } else {
+    console.error("Firestore connection failed");
+    return res.status(500).send("Firestore connection failed");
+  }
+}
