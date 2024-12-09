47b5a9c50a9fa1892916c1d7a8a867d151f1521f
jscht <80654820+jscht@users.noreply.github.com>
2024-12-10 00:38:40 +0900

    크롤링 요청 관련 로직 개선
    - 요청 리소스 제한 코드 추가
    - while -> for문으로 변경, 미리 전체 페이지네이션 총 개수를 구하고 반복문 수행
    - 요청 속도 제한 코드 추가

--- functions/src/crawlers/sriagent/recruitScraper.ts
+++ functions/src/crawlers/sriagent/recruitScraper.ts
@@ -2,6 +2,14 @@ import playwright from "playwright";
 import { ResponseRecruitData } from "../../types/responseRecruitData";
 import { extractRecruitData } from "./extractRecruitData";
 import { isNextPageAvailable } from "./isNextPageAvailable";
+import { getPaginationItemCount } from "./getPaginationItemCount";
+
+// 1초에서 3초 사이의 랜덤 대기 시간
+function getDelay() {
+  const min: number = 1000;
+  const max: number = 3000;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export async function recruitScraper() {
   const browser = await playwright.chromium.launch({
@@ -12,12 +20,24 @@ export async function recruitScraper() {
   try {
     const page = await browser.newPage();
 
+    await page.route("**/*", (route, request) => {
+      if (request.resourceType() === "image" || 
+        request.resourceType() === "stylesheet" || 
+        request.resourceType() === "font") {
+        route.abort();
+      } else {
+        route.continue();
+      }
+    });
+
     const targetUrl = `${process.env.SRI_URL}/jobs/`;
     await page.goto(targetUrl, { waitUntil: "networkidle" });
 
     const recruitData: ResponseRecruitData[] = [];
+    const totalPages = await getPaginationItemCount(page);
+    DebugLogger.server(`total page: ${totalPages.toString()}`);
 
-    while(true) {
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
       console.time("scraping");
       const extractRecruitList = await extractRecruitData(page);
       console.timeEnd("scraping");
@@ -29,16 +49,23 @@ export async function recruitScraper() {
 
       recruitData.push(...extractRecruitList);
 
-      const { nextPageButton, hasNextPage } = await isNextPageAvailable(page);
-      if (!hasNextPage) {
-        DebugLogger.server("Next page does not exist.");
-        break;
-      }
+      const delay = getDelay();
+      DebugLogger.server(`Waiting for ${delay}ms...`);
+      await page.waitForTimeout(delay);
 
-      await Promise.all([
-        nextPageButton.click(),
-        page.waitForLoadState("networkidle")
-      ]);
+      // 다음 페이지로 이동
+      if (currentPage < totalPages) {
+        const { nextPageButton, hasNextPage } = await isNextPageAvailable(page);
+        if (!hasNextPage) {
+          DebugLogger.server("Next page does not exist.");
+          break;
+        }
+
+        await Promise.all([
+          nextPageButton.click(),
+          page.waitForLoadState("networkidle")
+        ]);
+      }
     }
 
     return recruitData;
