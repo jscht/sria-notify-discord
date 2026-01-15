389626c2c51f67787b46fc3c4c6029f5b20287b5
jscht <80654820+jscht@users.noreply.github.com>
2026-01-16 08:56:05 +0900

    refactor: Unify events layer architecture
    - Move eventBus to src/events/eventBus
    - 새 위치에 맞춰 tsconfig.json path aliases경로 별칭 업데이트
    - events/index.ts에서 내보내기 구조 통합
    - 관련 파일들의 import path 일괄 수정

--- functions/src/common/utils/errorHandler.ts
+++ functions/src/common/utils/errorHandler.ts
@@ -6,8 +6,8 @@
 
 import { SystemError, ErrorLevel, ErrorCategory } from "./systemError";
 import { createLogger } from "./systemLogger";
-import { eventBus } from "@/eventBus/EventBus";
-import { EventType } from "@/eventBus/types";
+import { eventBus } from "@/events/eventBus/EventBus";
+import { EventType } from "@/events/eventBus/types";
 
 const logger = createLogger("ErrorHandler");
 
--- functions/src/events/index.ts
+++ functions/src/events/index.ts
@@ -1,7 +1,20 @@
+/**
+ * Events 레이어 통합 진입점
+ *
+ * Discord 이벤트 핸들러와 EventBus를 통합 관리합니다.
+ */
+
 import { onInteraction } from "./onInteraction";
 import { onPingPongCreate } from "./onPingPongCreate";
 import { onReady } from "./onReady";
 
+// EventBus 재export
+export { eventBus, EventBus } from "./eventBus/EventBus";
+export * from "./eventBus/types";
+export * from "./eventBus/constants";
+export { registerAllEventHandlers } from "./eventBus/utils/registerEventHandlers";
+
+// Discord 이벤트 핸들러 export
 export const events = [
   onReady(),
   onPingPongCreate(),
